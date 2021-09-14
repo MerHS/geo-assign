1a2f8a6b8b0e9dc04ed7cc276f19be73d9f16de6
MerHS <starvessel@naver.com>
2021-09-14 18:27:57 +0900

    draw basic biarc

--- iced-bezier/src/biarc.rs
+++ iced-bezier/src/biarc.rs
@@ -1,12 +1,13 @@
 use iced::{
     canvas::event::{self, Event},
-    canvas::{self, Canvas, Cursor, Frame, Geometry, Path, Stroke},
-    mouse, Color, Element, Length, Point, Rectangle,
+    canvas::{self, Cursor, Frame, Geometry, Path, Stroke},
+    mouse, Color, Point, Rectangle,
 };
 
 use crate::util::*;
 
 const RESOLUTION: usize = 100;
+const RES_4: usize = RESOLUTION / 4;
 const PTS_RADIUS: f32 = 3.0;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -19,6 +20,7 @@ pub enum Control {
 pub struct State {
     cache: canvas::Cache,
     curve: BezierCurve,
+    arcs: Vec<Biarc>,
     control: Control,
     pub is_dotted: bool,
     pub is_meshed: bool,
@@ -27,13 +29,18 @@ pub struct State {
 
 impl State {
     pub fn new() -> State {
+        let default_num_biarc = 1;
+        let curve = BezierCurve::default();
+        let mut arcs = Vec::<Biarc>::new();
+        curve.build_biarc(&mut arcs, default_num_biarc);
         State {
             cache: Default::default(),
-            curve: Default::default(),
+            curve,
+            arcs,
             control: Control::Static,
             is_dotted: false,
             is_meshed: true,
-            num_biarc: 2,
+            num_biarc: default_num_biarc,
         }
     }
 
@@ -53,6 +60,7 @@ impl State {
 
     pub fn set_num_biarc(&mut self, num_biarc: usize) {
         self.num_biarc = num_biarc;
+        self.curve.build_biarc(&mut self.arcs, self.num_biarc);
         self.request_redraw();
     }
 
@@ -81,13 +89,19 @@ impl State {
             Stroke::default(),
         );
 
+        // draw biarcs
+        if self.is_meshed {
+            for arc in self.arcs.iter() {
+                println!("{:?}", arc);
+                arc.draw(frame);
+            }
+        }
+
         // draw control points
         for ctr_point in self.curve.control_pts {
             let point_circ = Path::circle(ctr_point, PTS_RADIUS * 2.0);
             frame.fill(&point_circ, Color::from_rgba8(255, 0, 0, 1.0));
         }
-
-        // TODO: draw biarcs
     }
 }
 
@@ -98,11 +112,9 @@ impl<Message> canvas::Program<Message> for State {
         bounds: Rectangle,
         cursor: Cursor,
     ) -> (event::Status, Option<Message>) {
-        let cursor_position = if let Some(position) = cursor.position_in(&bounds) {
-            position
-        } else {
+        if cursor.position_in(&bounds).is_none() {
             return (event::Status::Ignored, None);
-        };
+        }
 
         match event {
             Event::Mouse(mouse_event) => match mouse_event {
@@ -139,6 +151,7 @@ impl<Message> canvas::Program<Message> for State {
                         };
                         self.control = Control::Moving(idx, pts);
                         self.curve.control_pts[idx] = pts;
+                        self.curve.build_biarc(&mut self.arcs, self.num_biarc);
                     }
                     (event::Status::Captured, None)
                 }
@@ -155,7 +168,7 @@ impl<Message> canvas::Program<Message> for State {
         }
     }
 
-    fn draw(&self, bounds: Rectangle, cursor: Cursor) -> Vec<Geometry> {
+    fn draw(&self, bounds: Rectangle, _cursor: Cursor) -> Vec<Geometry> {
         if Control::Static == self.control {
             let content = self.cache.draw(bounds.size(), |frame: &mut Frame| {
                 self.draw_frame(frame);
@@ -178,7 +191,7 @@ impl<Message> canvas::Program<Message> for State {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub struct BezierCurve {
     control_pts: [Point; 4],
 }
@@ -206,7 +219,7 @@ impl BezierCurve {
             }
         });
 
-        frame.stroke(&curve, Stroke::default().with_width(2.0));
+        frame.stroke(&curve, Stroke::default().with_width(1.2));
     }
 
     fn cubic_curve_to(&self, point: &mut Point, t: f32) -> () {
@@ -217,12 +230,92 @@ impl BezierCurve {
         let b1 = 3.0 * t_inv_sq * t;
         let b2 = 3.0 * t_inv * t_sq;
         let b3 = t_sq * t;
-
         point_clear(point);
-        point_add_weight_vec(point, b0, self.control_pts[0]);
-        point_add_weight_vec(point, b1, self.control_pts[1]);
-        point_add_weight_vec(point, b2, self.control_pts[2]);
-        point_add_weight_vec(point, b3, self.control_pts[3]);
+        point_add_weight_vec(point, b0, &self.control_pts[0]);
+        point_add_weight_vec(point, b1, &self.control_pts[1]);
+        point_add_weight_vec(point, b2, &self.control_pts[2]);
+        point_add_weight_vec(point, b3, &self.control_pts[3]);
+    }
+
+    fn cubic_deriv_to(&self, point: &mut Point, t: f32) -> () {
+        let t_inv = 1.0 - t;
+
+        let b0 = 3.0 * t_inv * t_inv;
+        let b1 = 6.0 * t * t_inv;
+        let b2 = 3.0 * t * t;
+        let p0 = &self.control_pts[0];
+        let p1 = &self.control_pts[1];
+        let p2 = &self.control_pts[2];
+        let p3 = &self.control_pts[3];
+        point_clear(point);
+        point.x = b0 * (p1.x - p0.x) + b1 * (p2.x - p1.x) + b2 * (p3.x - p2.x);
+        point.y = b0 * (p1.y - p0.y) + b1 * (p2.y - p1.y) + b2 * (p3.y - p2.y);
+    }
+
+    fn build_biarc(&self, arcs: &mut Vec<Biarc>, num_biarc: usize) -> () {
+        let mut pow_biarc = 2;
+        for _ in 1..num_biarc {
+            pow_biarc *= 2;
+        }
+
+        let len = arcs.len();
+        if pow_biarc != len {
+            arcs.resize(pow_biarc, Biarc::default());
+        }
+
+        let delta = 1.0 / (pow_biarc as f32);
+        let mut u0 = Point::default();
+        let mut u1 = Point::default();
+        let mut mid0 = Point::default();
+        let mut mid1 = Point::default();
+        let mut v0 = Point::default();
+        let mut v1 = Point::default();
+        let mut center = Point::default();
+        for (i, arc) in arcs.iter_mut().enumerate() {
+            let t = delta * (i as f32);
+            self.cubic_curve_to(&mut arc.start, t);
+            self.cubic_deriv_to(&mut u0, t);
+            normalize(&mut u0);
+
+            self.cubic_curve_to(&mut arc.end, t + delta);
+            self.cubic_deriv_to(&mut u1, t + delta);
+            normalize(&mut u1);
+
+            // calculate the center of joint circle
+            mid0.x = (arc.start.x + arc.end.x) / 2.0;
+            mid0.y = (arc.start.y + arc.end.y) / 2.0;
+            mid1.x = (arc.start.x + u0.x + arc.end.x + u1.x) / 2.0;
+            mid1.y = (arc.start.y + u0.y + arc.end.y + u1.y) / 2.0;
+            v0.x = arc.end.y - arc.start.y;
+            v0.y = -(arc.end.x - arc.start.x);
+            v1.x = (arc.end.y + u1.y) - (arc.start.y + u0.y);
+            v1.y = (arc.start.x + u0.x) - (arc.end.x + u1.x);
+            ray_intersection(&mid0, &v0, &mid1, &v1, &mut center);
+
+            // calculate radius and control point
+            let radius = distance(&center, &arc.start);
+            let theta = vec_angle(&center, &mid0);
+            arc.control.x = center.x + (radius * theta.cos()) as f32;
+            arc.control.y = center.y + (radius * theta.sin()) as f32;
+
+            // calculate the center of left arc
+            mid0.x = (arc.start.x + arc.control.x) / 2.0;
+            mid0.y = (arc.start.y + arc.control.y) / 2.0;
+            v0.x = u0.y;
+            v0.y = -u0.x;
+            v1.x = arc.control.y - arc.start.y;
+            v1.y = -(arc.control.x - arc.start.x);
+            ray_intersection(&arc.start, &v0, &mid0, &v1, &mut arc.center_left);
+
+            // calculate the center of right arc
+            mid1.x = (arc.end.x + arc.control.x) / 2.0;
+            mid1.y = (arc.end.y + arc.control.y) / 2.0;
+            v1.x = u1.y;
+            v1.y = -u1.x;
+            v0.x = arc.control.y - arc.end.y;
+            v0.y = -(arc.control.x - arc.end.x);
+            ray_intersection(&arc.end, &v1, &mid1, &v0, &mut arc.center_right);
+        }
     }
 }
 
@@ -239,34 +332,98 @@ impl Default for BezierCurve {
     }
 }
 
-// #[derive(Debug, Clone, Copy)]
-// enum DragState {
-//     One { from: Point },
-//     Two { from: Point, to: Point },
-// }
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Biarc {
+    start: Point,
+    control: Point,
+    end: Point,
+    center_left: Point,
+    center_right: Point,
+}
 
-// impl Pending {
-//     fn draw(&self, bounds: Rectangle, cursor: Cursor) -> Geometry {
-//         let mut frame = Frame::new(bounds.size());
+impl Biarc {
+    fn draw(&self, frame: &mut Frame) {
+        let left_curve = Path::new(|p| {
+            let start = self.start;
+            let control = self.control;
+            let center_left = self.center_left;
+            // draw left
+            let mid_left = Point {
+                x: (control.x + start.x) / 2.0,
+                y: (control.y + start.y) / 2.0,
+            };
+            let radius_left = distance(&center_left, &start);
+            let theta_left = vec_angle(&center_left, &start);
+            let theta_mid = vec_angle(&center_left, &mid_left);
+            let theta_cnt = vec_angle(&center_left, &control);
 
-//         if let Some(cursor_position) = cursor.position_in(&bounds) {
-//             match *self {
-//                 Pending::One { from } => {
-//                     let line = Path::line(from, cursor_position);
-//                     frame.stroke(&line, Stroke::default().with_width(2.0));
-//                 }
-//                 Pending::Two { from, to } => {
-//                     let curve = Curve {
-//                         from,
-//                         to,
-//                         control: cursor_position,
-//                     };
+            Biarc::draw_arc(p, &center_left, radius_left as f32, theta_left, theta_mid);
+            Biarc::draw_arc(p, &center_left, radius_left as f32, theta_mid, theta_cnt);
+        });
 
-//                     Curve::draw_all(&[curve], &mut frame);
-//                 }
-//             };
-//         }
+        let right_curve = Path::new(|p| {
+            let control = self.control;
+            let end = self.end;
+            let center_right = self.center_right;
+            // draw left
+            let mid_right = Point {
+                x: (control.x + end.x) / 2.0,
+                y: (control.y + end.y) / 2.0,
+            };
+            let radius_right = distance(&center_right, &end);
+            let theta_cnt = vec_angle(&center_right, &control);
+            let theta_mid = vec_angle(&center_right, &mid_right);
+            let theta_right = vec_angle(&center_right, &end);
 
-//         frame.into_geometry()
-//     }
-// }
+            Biarc::draw_arc(p, &center_right, radius_right as f32, theta_cnt, theta_mid);
+            Biarc::draw_arc(
+                p,
+                &center_right,
+                radius_right as f32,
+                theta_mid,
+                theta_right,
+            );
+        });
+
+        frame.stroke(
+            &left_curve,
+            Stroke::default()
+                .with_width(3.0)
+                .with_color(Color::from_rgba8(40, 210, 0, 1.0)),
+        );
+        frame.stroke(
+            &right_curve,
+            Stroke::default()
+                .with_width(3.0)
+                .with_color(Color::from_rgba8(30, 0, 210, 1.0)),
+        );
+    }
+
+    fn draw_arc(
+        p: &mut iced::canvas::path::Builder,
+        center: &Point,
+        radius: f32,
+        theta0: f64,
+        theta1: f64,
+    ) -> () {
+        let mut angle0 = theta0;
+        let mut angle1 = theta1;
+        if (angle0 < 0.0 && 0.0 <= angle1) || (angle0 >= 0.0 && 0.0 > angle1) {
+            angle0 += 2.0 * std::f64::consts::PI;
+            angle1 += 2.0 * std::f64::consts::PI;
+        }
+
+        let mut point = Point {
+            x: center.x + radius * (angle0.cos() as f32),
+            y: center.y + radius * (angle0.sin() as f32),
+        };
+        let delta = (angle1 - angle0) / (RES_4 as f64);
+        p.move_to(point);
+        for _ in 1..=RES_4 {
+            angle0 += delta;
+            point.x = center.x + radius * (angle0.cos() as f32);
+            point.y = center.y + radius * (angle0.sin() as f32);
+            p.line_to(point);
+        }
+    }
+}
--- iced-bezier/src/util.rs
+++ iced-bezier/src/util.rs
@@ -5,7 +5,7 @@ pub fn point_clear(point: &mut Point) -> () {
     point.y = 0.0;
 }
 
-pub fn point_add_weight_vec(point: &mut Point, weight: f32, vec: Point) {
+pub fn point_add_weight_vec(point: &mut Point, weight: f32, vec: &Point) {
     point.x += weight * vec.x;
     point.y += weight * vec.y;
 }
@@ -15,7 +15,8 @@ pub fn point_add_weight_vec(point: &mut Point, weight: f32, vec: Point) {
 /// # Arguments
 /// * `p0`, `p1`: Initial points
 /// * `v0`, `v1`: Vector; No needs to be a unit vector.
-pub fn ray_intersection(p0: &Point, v0: &Point, p1: &Point, v1: &Point) -> Point {
+/// * `to`: save to this.
+pub fn ray_intersection(p0: &Point, v0: &Point, p1: &Point, v1: &Point, to: &mut Point) -> () {
     let mut determinant = v0.x * v1.y - v1.x * v0.y;
     if determinant == 0.0 {
         determinant += 0.0001;
@@ -24,12 +25,38 @@ pub fn ray_intersection(p0: &Point, v0: &Point, p1: &Point, v1: &Point) -> Point
     let x = p0.x + scalar * v0.x;
     let y = p0.y + scalar * v0.y;
 
-    Point { x, y }
+    to.x = x;
+    to.y = y;
 }
 
 /// Calculate distance of two vector
-pub fn distance(p0: &Point, p1: &Point) -> f32 {
+pub fn distance(p0: &Point, p1: &Point) -> f64 {
     let x = (p1.x - p0.x) as f64;
     let y = (p1.y - p0.y) as f64;
-    f64::sqrt(x * x + y * y) as f32
+    f64::sqrt(x * x + y * y)
+}
+
+/// Calculate angle align to x-axis
+/// return (-pi, pi]
+pub fn vec_angle(center: &Point, vec: &Point) -> f64 {
+    if vec.x == center.x {
+        if vec.y >= vec.x {
+            std::f64::consts::FRAC_PI_2
+        } else {
+            -std::f64::consts::FRAC_PI_2
+        }
+    } else {
+        let tan = ((vec.y - center.y) / (vec.x - center.x)) as f64;
+        tan.atan()
+    }
+}
+
+pub fn norm(point: &Point) -> f32 {
+    f64::sqrt((point.x * point.x + point.y * point.y) as f64) as f32
+}
+
+pub fn normalize(point: &mut Point) -> () {
+    let len = norm(point);
+    point.x = point.x / len;
+    point.y = point.y / len;
 }
